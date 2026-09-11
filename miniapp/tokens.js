492201f7c1e59660d51d3f2fda29a4f432d3900e
tron <git@amvera.ru>
2026-09-11 15:17:29 +0000

    deleted miniapp

--- miniapp/tokens.js
+++
@@ -1,1 +0,0 @@
-window.designReady=fetch('/app/assets/EAGENT_DESIGN_TOKENS.json').then(r=>{if(!r.ok)throw Error('Design tokens unavailable');return r.json()}).then(tokens=>{for(const [name,value] of Object.entries(tokens))document.documentElement.style.setProperty('--'+name,value)});
